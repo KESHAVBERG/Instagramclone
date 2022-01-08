250b0be7952e854a6a5873a92e2bfd13d9edf565
KESHAVBERG <kingkeshav7@gmail.com>
2022-01-08 09:52:14 +0530

    starting to work on post

--- lib/consts/stories.dart
+++ lib/consts/stories.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Container storiesView(String imgPath) {
+Container storiesView(String imgPath, String userName) {
   return Container(
     width: 80,
     margin: EdgeInsets.symmetric(horizontal: 1),
@@ -45,26 +45,24 @@ Container storiesView(String imgPath) {
     //   ),
     //
     // ),
-      child: Column(
-        children: [
-          Container(
+    child: Column(
+      children: [
+        Container(
           height: 70,
           width: 70,
           alignment: Alignment.center,
-          margin:const EdgeInsets.symmetric(horizontal: 2),
-          decoration:const BoxDecoration(
+          margin: const EdgeInsets.symmetric(horizontal: 2),
+          decoration: const BoxDecoration(
             gradient: LinearGradient(colors: [
               // Color.fromRGBO(88,81,219,1),
               // Color.fromRGBO(131,58,180,1),
-              Color.fromRGBO(247,119,55,1),
-              Color.fromRGBO(252,175,69,1),
-              Color.fromRGBO(255,220,128,1),
-              Color.fromRGBO(253,29,29,1),
+              Color.fromRGBO(247, 119, 55, 1),
+              Color.fromRGBO(252, 175, 69, 1),
+              Color.fromRGBO(255, 220, 128, 1),
+              Color.fromRGBO(253, 29, 29, 1),
 
-              Color.fromRGBO(193,53,132,1),
-              Color.fromRGBO(225,48,108,1),
-
-
+              Color.fromRGBO(193, 53, 132, 1),
+              Color.fromRGBO(225, 48, 108, 1),
             ]),
             shape: BoxShape.circle,
           ),
@@ -72,52 +70,46 @@ Container storiesView(String imgPath) {
             height: 65,
             width: 65,
             alignment: Alignment.center,
-            decoration:const BoxDecoration(
+            decoration: const BoxDecoration(
               color: Colors.white,
               shape: BoxShape.circle,
             ),
             child: Container(
               height: 60,
               width: 60,
-              decoration:BoxDecoration(
-                color: Colors.green,
+              decoration: BoxDecoration(
+                  color: Colors.green,
                   shape: BoxShape.circle,
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(imgPath)
-                )
-              ),
+                  image: DecorationImage(
+                      fit: BoxFit.fill, image: AssetImage(imgPath))),
             ),
           ),
-
         ),
-          const Text("UserName", overflow: TextOverflow.ellipsis,style: TextStyle(
-            fontWeight: FontWeight.w400
-          ),)
-        ],
-      ),
-
+        const SizedBox(height: 6,),
+        Text(
+          userName,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontFamily: "Roboto",fontSize: 12,fontWeight: FontWeight.w500),
+        )
+      ],
+    ),
   );
 }
 
-
-Widget storiesToView(context){
-  return  Container(
-    padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+Widget storiesToView(context) {
+  return Container(
+    padding:const EdgeInsets.fromLTRB(5, 0, 0, 0),
     width: double.infinity,
     height: 100,
     child: ListView(
       scrollDirection: Axis.horizontal,
       children: [
-        storiesView("imgs/userprofilepic.jpg"),
-        storiesView("profilepics/p1.jpg"),
-        storiesView("profilepics/p2.jpg"),
-        storiesView("profilepics/p3.jpg"),
-        storiesView("profilepics/p1.jpg"),
-        storiesView("profilepics/p2.jpg"),
-
-
-
+        storiesView("imgs/userprofilepic.jpg", "Your Story"),
+        storiesView("profilepics/p1.jpg", "TheRock"),
+        storiesView("profilepics/p2.jpg","Tom"),
+        storiesView("profilepics/p3.jpg","sr.keshav" ),
+        storiesView("profilepics/p1.jpg", "chrishemsworth"),
+        storiesView("profilepics/p2.jpg","chrisevans"),
       ],
     ),
   );
--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:functionlearner/consts/stories.dart';
 import 'package:functionlearner/my_flutter_app_icons.dart';
@@ -15,81 +14,79 @@ class _FeedState extends State<Feed> {
   Widget build(BuildContext context) {
     final w = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: BottomAppBar(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Icon(
-                Icons.home,
-                color: Colors.black,
-                size: 30,
-              ),
-              const Icon(
-                Icons.search,
-                color: Colors.black,
-                size: 30,
-              ),
-              Container(
-                height: 30,
-                width: 30,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("imgs/reelsiconpng.png"))),
-              ),
-              const Icon(
-                Icons.favorite_border,
-                color: Colors.black,
-                size: 30,
-              ),
-              Container(
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: AssetImage("imgs/userprofilepic.jpg"))),
-              )
-            ],
-          ),
-        ),
-      ),
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 0.0,
-        title: Image.asset(
-          "imgs/icon.png",
-          height: 50,
-        ),
-        actions:  [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 3.0),
-            child: Icon(
-              MyFlutterApp.plus_squared_alt,
-              color: Colors.black,
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  Icons.home,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                const Icon(
+                  Icons.search,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                Container(
+                  height: 30,
+                  width: 30,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("imgs/reelsiconpng.png"))),
+                ),
+                const Icon(
+                  Icons.favorite_border,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage("imgs/userprofilepic.jpg"))),
+                )
+              ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15.0),
-            child: Container(
-              height: 30,
-              width: 30,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.contain,
-                  image: AssetImage('imgs/messager icon.jpg')
-                )
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Image.asset(
+            "imgs/icon.png",
+            height: 50,
+          ),
+          actions: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 3.0),
+              child: Icon(
+                MyFlutterApp.plus_squared_alt,
+                color: Colors.black,
               ),
-            )
-          )
-        ],
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Container(
+                  height: 30,
+                  width: 30,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.contain,
+                          image: AssetImage('imgs/messager icon.jpg'))),
+                ))
+          ],
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
           child: ListView(
             children: [
               storiesToView(context),
@@ -97,11 +94,11 @@ class _FeedState extends State<Feed> {
                 height: 350,
                 width: MediaQuery.of(context).size.width,
                 color: Colors.green,
+                child: const Text("Style",
+                    style: TextStyle(fontFamily: "Roboto", fontSize: 15)),
               ),
             ],
           ),
-
-      )
-    );
+        ));
   }
 }
